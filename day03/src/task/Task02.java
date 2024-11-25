630391057e6042151111148c66da555914761a0b
soooohyeon <dyddyd310@gmail.com>
2024-11-25 21:42:59 +0900

    241125 변수, 자료형변환 과제 완료

--- day03/src/task/Task02.java
+++ day03/src/task/Task02.java
@@ -3,12 +3,12 @@ package task;
 public class Task02 {
    public static void main(String[] args) {
       //int, long, double, float, boolean, char 선언 및 초기화
-	   int intVal = 17;
+	   int intVal = 65;
 	   long longVal = 17L;
 	   double doubleVal = 3.14;
 	   float floatVal = 3.14f;
 	   boolean booleanVal = false;
-	   char charVal = 'A';
+	   char charVal = 'F';
 	   
       //1. 정수(int) -> 모든 자료형 자동형변환 가능여부 확인
 	   long longVal1 = intVal;
@@ -20,19 +20,41 @@ public class Task02 {
 	   System.out.println("long 타입 : " + longVal1);
 	   System.out.println("double 타입 : " + doubleVal1);
 	   System.out.println("float 타입 : " + floatVal1);
-	   System.out.println("결과 : long, double, float 타입만 가능");
+	   System.out.println("결과 : long, double, float 타입 가능\n");
       
       //2. 정수(long) -> 모든 자료형 자동형변환 가능여부 확인
-	   System.out.println("[2. 정수(long)의 모든 자료형 형변환 가능여부]");
+	   double doubleVal2 = longVal;
 	   
-	   System.out.println("결과 :  타입만 가능");
+	   System.out.println("[2. 정수(long)의 모든 자료형 형변환 가능여부]");
+	   System.out.println("double 타입 : " + doubleVal2);
+	   System.out.println("결과 : double 타입 가능 \n");
       
       //3. 실수(double) -> 모든 자료형 자동형변환 가능여부 확인
+	   System.out.println("[3. 실수(double)의 모든 자료형 형변환 가능여부]");
+	   System.out.println("결과 : 자동형변환 가능한 타입 없음\n");
       
       //4. 실수(float) -> 모든 자료형 자동형변환 가능여부 확인
+	   double doubleVal4 = floatVal;
+	   
+	   System.out.println("[4. 실수(float)의 모든 자료형 형변환 가능여부]");
+	   System.out.println("double 타입 : " + doubleVal4);
+	   System.out.println("결과 : double 타입 가능\n");
       
       //5. 논리형 (boolean)  -> 모든 자료형 자동형변환 가능여부 확인
+	   System.out.println("[5. 논리형(boolean)의 모든 자료형 형변환 가능여부]");
+	   System.out.println("결과 : 자동형변환 가능한 타입 없음\n");
       
       //6. 문자형(char) -> 모든 자료형 자동형변환 가능여부 확인
+	   int intVal6 = charVal;
+	   long longVal6 = charVal;
+	   double doubleVal6 = charVal;
+	   float floatVal6 = charVal;
+	   
+	   System.out.println("[6. 문자형(char)의 모든 자료형 형변환 가능여부]");
+	   System.out.println("int 타입 : " + intVal6);
+	   System.out.println("long 타입 : " + longVal6);
+	   System.out.println("double 타입 : " + doubleVal6);
+	   System.out.println("float 타입 : " + floatVal6);
+	   System.out.println("결과 : int, long, double, float 타입 가능");
    }
 }
--- day03/src/task/Task03.java
+++ day03/src/task/Task03.java
@@ -3,28 +3,61 @@ package task;
 public class Task03 {
    public static void main(String[] args) {
       //int, long, double, float, boolean, char 선언 및 초기화
-	   int intVal = 17;
-	   long longVal = 17L;
-	   double doubleVal = 3.14;
-	   float floatVal = 3.14f;
+	   int intVal = 67;
+	   long longVal = 68L;
+	   double doubleVal = 69.14;
+	   float floatVal = 72.14f;
 	   boolean booleanVal = false;
-	   char charVal = 'A';
+	   char charVal = 'Z';
 	   
       //1. 정수(int) -> 모든 자료형 강제형변환 가능여부 확인
-	   System.out.println("[1. 정수의 모든 자료형 강제형변환 가능여부]");
+	   System.out.println("[1. 정수(int)의 모든 자료형 강제형변환 가능여부]");
 	   
 	   System.out.println("long 타입 : " + (long)intVal);
 	   System.out.println("double 타입 : " + (double)intVal);
 	   System.out.println("float 타입 : " + (float)intVal);
+	   System.out.println("char 타입 : " + (char)intVal);
+	   System.out.println("결과 : long, double, float, char 타입 가능\n");
       
       //2. 정수(long) -> 모든 자료형 강제형변환 가능여부 확인
+	   System.out.println("[2. 정수(long)의 모든 자료형 강제형변환 가능여부]");
+	   
+	   System.out.println("int 타입 : " + (int)longVal);
+	   System.out.println("double 타입 : " + (double)longVal);
+	   System.out.println("float 타입 : " + (float)longVal);
+	   System.out.println("char 타입 : " + (char)longVal);
+	   System.out.println("결과 : long, double, float, char 타입 가능\n");
       
       //3. 실수(double) -> 모든 자료형 강제형변환 가능여부 확인
+	   System.out.println("[3. 실수(double)의 모든 자료형 강제형변환 가능여부]");
+	   
+	   System.out.println("int 타입 : " + (int)doubleVal);
+	   System.out.println("long 타입 : " + (long)doubleVal);
+	   System.out.println("float 타입 : " + (float)doubleVal);
+	   System.out.println("char 타입 : " + (char)doubleVal);
+	   System.out.println("결과 : long, long, float, char 타입 가능\n");
       
       //4. 실수(float) -> 모든 자료형 강제형변환 가능여부 확인
+	   System.out.println("[4. 실수(float)의 모든 자료형 강제형변환 가능여부]");
+	   
+	   System.out.println("int 타입 : " + (int)floatVal);
+	   System.out.println("long 타입 : " + (long)floatVal);
+	   System.out.println("double 타입 : " + (double)floatVal);
+	   System.out.println("char 타입 : " + (char)floatVal);
+	   System.out.println("결과 : long, long, double, char 타입 가능\n");
       
       //5. 논리형 (boolean) -> 모든 자료형 강제형변환 가능여부 확인
+	   System.out.println("[5. 논리형(boolean)의 모든 자료형 강제형변환 가능여부]");
+	   System.out.println("결과 : 자동형변환 가능한 타입 없음 \n");
       
       //6. 문자형(char) -> 모든 자료형 강제형변환 가능여부 확인
+	   System.out.println("[6. 문자형(char)의 모든 자료형 강제형변환 가능여부]");
+	   
+	   System.out.println("int 타입 : " + (int)charVal);
+	   System.out.println("long 타입 : " + (long)charVal);
+	   System.out.println("double 타입 : " + (double)charVal);
+	   System.out.println("float 타입 : " + (float)charVal);
+	   System.out.println("결과 : long, long, double, float 타입 가능");
+	   
    }
 }
